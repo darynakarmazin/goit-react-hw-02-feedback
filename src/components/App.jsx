63c1c1c309539fe16160126db05be794b2ccdb8f
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-05-22 19:09:31 +0300

    Added class App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 3,
+    neutral: 2,
+    bad: 2,
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+        100
+    );
+  };
+
+  handleIncrement = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <p>Please leave feed back</p>
+          <button type="button" onClick={this.handleIncrement}>
+            Good
+          </button>
+          <button type="button" onClick={this.handleIncrement}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.handleIncrement}>
+            Bad
+          </button>
+        </div>
+        <div>
+          <p>Statistics</p>
+          <ul>
+            <li>Good: {this.state.good}</li>
+            <li>Neutral: {this.state.neutral}</li>
+            <li>Bad: {this.state.bad}</li>
+            <li>Total: {this.countTotalFeedback()}</li>
+            <li>
+              Positive feedback: {this.countPositiveFeedbackPercentage()}%
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
